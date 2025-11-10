a0477625b4184502491537f87bd883b324ea0bf0
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-11-10 08:14:44 -0500

    Wrote linked list constructor + first method

---
+++ src/linked-list.js
@@ -0,0 +1,23 @@
+// Linked list class
+
+import { Node } from "node.js";
+
+export class linkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+}
