a00e6ceda29975fad8f1e88c7c540ab699a03446
Joe Kaufman <[joe.kaufman527@gmail.com]>
2025-11-10 19:56:11 -0500

    testing append method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 // Template index.js file
 
-import "./style.css";
+import { LinkedList } from "./linked-list.js";
+
+const linkedList = new LinkedList();
+linkedList.append("testing");
+linkedList.append("testing");
+linkedList.append("123");
+linkedList.append("456");
+
+console.log(linkedList);
